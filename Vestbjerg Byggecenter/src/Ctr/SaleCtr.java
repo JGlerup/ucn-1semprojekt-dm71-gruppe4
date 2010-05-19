0b28ea25c30503ad2fdc3d07d7a206d6dcde8528
glerup9@hotmail.com <glerup9@hotmail.com@b6c74990-cafa-bb58-6d04-158552be35c7>
2010-05-19 09:27:20 +0000

    salesContainer added, sale lavet lidt

--- Vestbjerg Byggecenter/src/Ctr/SaleCtr.java
+++ Vestbjerg Byggecenter/src/Ctr/SaleCtr.java
@@ -5,10 +5,39 @@
 
 package Ctr;
 
+import Model.Sale;
+import Model.SalesContainer;
+import Model.SalesLineItem;
+
 /**
  *
  * @author Daniel
  */
 public class SaleCtr {
+    private SalesContainer salesContainer;
+    private Sale salesLineItemList;
+    
+    public SaleCtr()
+    {
+        salesContainer = SalesContainer.getInstance();
+    }
+    
+    public void createSale(String newDate, int newPrice, int newQuantity, int newTotalPrice)
+    {
+        int index = 0;
+        Sale saleObjekt = new Sale(newDate, newPrice);
+        salesContainer.createSale(saleObjekt);
+        SalesLineItem sLIObjekt = new SalesLineItem(newQuantity, newTotalPrice);
+        index = salesContainer.listSales().size() -1;
+        salesLineItemList.createSalesLineItem(sLIObjekt);
+    }
+
+    public void createSalesLineItem(int newQuantity, int newTotalPrice)
+    {
+        int index = 0;
+        SalesLineItem sLIObjekt = new SalesLineItem(newQuantity, newTotalPrice);
+        index = salesContainer.listSales().size() -1;
+        salesLineItemList.createSalesLineItem(sLIObjekt);
+    }
 
 }
--- Vestbjerg Byggecenter/src/Model/Sale.java
+++ Vestbjerg Byggecenter/src/Model/Sale.java
@@ -5,6 +5,8 @@
 
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Daniel
@@ -14,6 +16,34 @@ public class Sale {
     private String date;
     private int price;
     private boolean order;
+    private ArrayList<SalesLineItem> sLIList;
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Sale(String date, int price)
+    {
+        this.date = date;
+        this.price = price;
+        sLIList = new ArrayList<SalesLineItem>();
+    }
+
+    public void createSalesLineItem(SalesLineItem newSalesLineItem)
+    {
+        sLIList.add(newSalesLineItem);
+    }
 
 }
